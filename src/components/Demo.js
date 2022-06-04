40380a67bae081bdd16f3e2a803d38e3345ede5d
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-04 11:10:51 +0530

    demo

---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                pallav
+            </div>
+        );
+    }
+}
+
+export default Demo;
