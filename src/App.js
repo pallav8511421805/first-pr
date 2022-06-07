97aad00c685165842c7289719472a09dcae89317
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-07 19:10:21 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Demo from './components/Demo';
-import City from './containers/City';
-import Cityfun from './containers/Cityfun';
-import Contry from './containers/Contry';
-import Countryfun from './containers/Countryfun';
-import Time from './components/Time';
-import Timefun from './containers/Timefun';
 import Counter from './components/Counter';
 
 function App() {
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter(props) {
+    const [counter, setcounter] = useState(0);
+    const plus = () => {
+        setcounter(counter + 1);
+    }
+    const minus = () => {
+        setcounter(counter - 1);
+    }
     return (
         <div>
-            <p></p>
-            <div style={{display:"flex",justifyContent:"center"}}>
-            <button style={{color:"white",background:"red",border:"none",width:"120px",height:"35px",display:"block",borderRadius:"5px",margin:"15px"}}>Decrement</button>
-            <button style={{color:"white",background:"green",border:"none",width:"120px",height:"35px",display:"block",borderRadius:"5px",margin:"15px"}}>Increment</button>
+            <h1 style={{ display: "flex", justifyContent: "center" }}>{"counter = " + counter}</h1>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <button style={{ color: "white", background: "green", border: "none", width: "120px", height: "35px", display: "block", borderRadius: "5px", margin: "15px" }} onClick={() => plus()}>Increment</button>
+                <button style={{ color: "white", background: "red", border: "none", width: "120px", height: "35px", display: "block", borderRadius: "5px", margin: "15px" }} onClick={() => minus()}>Decrement</button>
             </div>
         </div>
     );
