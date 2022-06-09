a4f4b7f500f030d39dde88f8d7d0678a4a6ec235
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-09 17:42:01 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,30 +3,32 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Loading from './Hoc/Loading';
 import Home from './Hoc/Home';
+import Counter from './components/Counter';
 
-const Homeloadingdata = Loading(Home);
+// const Homeloadingdata = Loading(Home);
 
 function App() {
-  const [Loading, setloading] = useState(false);
-  const [data, setdata] = useState([]);
+  // const [Loading, setloading] = useState(false);
+  // const [data, setdata] = useState([]);
 
-  const orgdata = [{
-    id: 101, name: "Amit"
-  }, {
-    id: 102, name: "Ajay"
-  }];
+  // const orgdata = [{
+  //   id: 101, name: "Amit"
+  // }, {
+  //   id: 102, name: "Ajay"
+  // }];
 
-  useEffect(() => {
-    setloading(true)
-    setTimeout(() => { setloading(false); setdata(orgdata) }, 2000);
-  }, [])
+  // useEffect(() => {
+  //   setloading(true)
+  //   setTimeout(() => { setloading(false); setdata(orgdata) }, 2000);
+  // }, [])
 
   return (
     <div>
-      <Homeloadingdata 
+      {/* <Homeloadingdata 
       isloading={Loading} 
       data={data} 
-      />
+      /> */}
+    <Counter/>
     </div>
   );
 }
