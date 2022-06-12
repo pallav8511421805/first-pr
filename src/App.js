8bedf7d3b878d0df28f84ae80ee4641b2c810ad5
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-12 11:29:48 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-import Loading from './Hoc/Loading';
-import Home from './Hoc/Home';
-import Counter from './components/Counter';
-
-// const Homeloadingdata = Loading(Home);
 
 function App() {
-  // const [Loading, setloading] = useState(false);
-  // const [data, setdata] = useState([]);
-
-  // const orgdata = [{
-  //   id: 101, name: "Amit"
-  // }, {
-  //   id: 102, name: "Ajay"
-  // }];
-
-  // useEffect(() => {
-  //   setloading(true)
-  //   setTimeout(() => { setloading(false); setdata(orgdata) }, 2000);
-  // }, [])
-
+  
   return (
     <div>
-      {/* <Homeloadingdata 
-      isloading={Loading} 
-      data={data} 
-      /> */}
-    <Counter/>
+      
     </div>
   );
 }
