d8890e3fc27cf361cd0dc078951f388472a80440
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-12 11:34:25 +0530

    loginform

--- src/containers/Loginform.js
+++ src/containers/Loginform.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Button } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Loginform(props) {
     return (
         <div>
-            
+            <div>
+  <Button
+    color="primary"
+  >
+    Click Me
+  </Button>
+</div>
         </div>
     );
 }
